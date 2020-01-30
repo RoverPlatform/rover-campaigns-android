50298fa93f8b9a8a09f7622dc4c9da2628f4b020
Sam Woodall <samwoodall01@gmail.com>
2020-01-30 11:50:11 -0500

    Slight renaming to screenViewed function in Event Companion

--- core/src/main/kotlin/io/rover/campaigns/core/events/domain/Event.kt
+++ core/src/main/kotlin/io/rover/campaigns/core/events/domain/Event.kt
@@ -16,7 +16,7 @@ data class Event(
     ): this(name, attributes, Date(), UUID.randomUUID())
 
     companion object {
-        fun screenViewedEvent(screenName: String, screenLabel: String? = null, contentID: String? = null): Event {
+        fun screenViewed(screenName: String, screenLabel: String? = null, contentID: String? = null): Event {
             val attributes = mutableMapOf<String, Any>("screenName" to screenName)
 
             screenLabel?.let { attributes.put("screenLabel", it) }
