1e2b5fd368a7666b922daf26bbc21094d5ba79d9
Sam Woodall <samwoodall01@gmail.com>
2020-01-30 11:11:30 -0500

    Remove secondary constructor from event

--- core/src/main/kotlin/io/rover/campaigns/core/events/domain/Event.kt
+++ core/src/main/kotlin/io/rover/campaigns/core/events/domain/Event.kt
@@ -4,17 +4,6 @@ import io.rover.campaigns.core.data.domain.Attributes
 import java.util.Date
 import java.util.UUID
 
-
-private fun getScreenViewedAttributes(screenName: String, screenLabel: String?, contentID: String?): Map<String, Any> {
-    val attributes = mutableMapOf<String, Any>("screenName" to screenName)
-
-    screenLabel?.let { attributes.put("screenLabel", it) }
-    contentID?.let { attributes.put("contentID", contentID) }
-
-    return attributes
-}
-
-
 data class Event(
     val name: String,
     val attributes: Attributes,
@@ -26,11 +15,14 @@ data class Event(
         attributes: Attributes
     ): this(name, attributes, Date(), UUID.randomUUID())
 
-    constructor(screenName: String, screenLabel: String? = null, contentID: String? = null): this("Screen Viewed", getScreenViewedAttributes(screenName, screenLabel, contentID), Date(), UUID.randomUUID())
-
     companion object {
         fun screenViewedEvent(screenName: String, screenLabel: String? = null, contentID: String? = null): Event {
-            return Event(screenName, screenLabel, contentID)
+            val attributes = mutableMapOf<String, Any>("screenName" to screenName)
+
+            screenLabel?.let { attributes.put("screenLabel", it) }
+            contentID?.let { attributes.put("contentID", contentID) }
+
+            return Event("Screen Viewed", attributes)
         }
     }
 }
