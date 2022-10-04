93b6abb4d60c92b919e6d2d9d22b110061a7853e
Andrew Clunis <andrew@orospakr.ca>
2022-10-04 15:25:44 -0400

    Remove unnecessary shell script

--- get-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Interrogate the SDK for its version number.
-
-# usage: ./get-version.sh
-
-# Run the gradle target that prints out the version number and grep the value out.
-./gradlew printVersionNumber | grep ROVER_CAMPAIGNS_VERSION | cut -d '=' -f 2
